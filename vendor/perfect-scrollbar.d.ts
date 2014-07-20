2374e9762fda5e50bceab68b421518e4396898f9
Exsarge <benjikaiser@googlemail.com>
2014-07-20 23:02:01 +0200

    index.html  Added a local jquery-ui including the "Black Tie" Theme. (White on Black)
    /index/index.ts
     Reworked the index.ts to be a module instead of a class and init on cu.OnInitialized
     Added the ui name to each element on hover.
    
    /index/index.css
     Repositioned Icons and changed there style.
    
    /cu/cu.ts
     If you send a message to a chatroom you are not "presence" in, you will automaticly join and resend the message, so you don't have to type it again.
    
    /chat/chat.html
     Added perfectScrollbar for a perfect scrollbar :)
     Added jquery cookie lib
    
    /chat/chat.css
     Added style for send whisper (to see what you send)
     Added some styles for perfect scrollbar
     Changed console font to "Courier New"
    /chat/chat.ts
     Added cookie support for saving loginToken into cookie so you don't have to reconnect on reload in the browser.
     Added chat commands for chat "/connect <loginToken>" "/disconnect"
     Changed whisper don't have to use full jid (eg. "exsarge" instead of "exsarge@chat.camelotunchained.com")
     Added chat command usage help on arguments length mismatch
     Added "OnChatCommand", at the moment only for /whisper. It displays your send message into your own chat too

---
+++ vendor/perfect-scrollbar.d.ts
@@ -0,0 +1,14 @@
+﻿// Type definitions for jQueryUI 1.9
+// Project: http://jqueryui.com/
+// Definitions by: Boris Yankov <https://github.com/borisyankov/>, John Reilly <https://github.com/johnnyreilly>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+
+/// <reference path="jquery.d.ts"/>
+
+declare module perfectScrollbar {
+}
+interface JQuery {
+    perfectScrollbar(): JQuery;
+    perfectScrollbar(options: any): JQuery;
+}
