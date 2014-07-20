ef6b561e436249e30bd139917cfd2396ec51144e
Mehuge <adf@sorcerer.co.uk>
2014-07-20 11:27:06 +0100

    Added base module-list.js (for Addons UI)
    Added base module-list.js which lists the core UIs (those shipped with
    the game) and addons UI.

---
+++ module-list.js
@@ -0,0 +1,15 @@
+﻿var UI = UI || {};
+UI.Modules = {
+    "character": { core: true },
+    "chat": { core: true },
+    "errormessages": { core: true },
+    "kills": { core: true },
+    "login": { core: true },
+    "perfhud": { core: true },
+    "respawn": { core: true },
+    "skillbar": { core: true },
+    "target": { core: true },
+    "addons": { core: false, autoLoad: true },
+    // custom-ui's go below...
+    // "example": { core: false, autoLoad: true }
+};
